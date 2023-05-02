f19ff8ef55674a4e6c68e7e0de099ff45ebba102
rafaede <124257126+rafaede@users.noreply.github.com>
2023-05-03 02:08:34 +0700

    Add files via upload

---
+++ get/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Data struct {
+	Name    string `json:"name"`
+	NIM     string `json:"nim"`
+	Address string `json:"address"`
+}
+
+func abc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 PAGE NOT FOUND", http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "forms.html")
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err : %v", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Post from website r.postfrom = %v\n", r.PostForm)
+		name := r.FormValue("name")
+		nim := r.FormValue("nim")
+		address := r.FormValue("address")
+
+		fmt.Fprintf(w, "Nama = %s\n", name)
+		fmt.Fprintf(w, "NIM = %s\n", nim)
+		fmt.Fprintf(w, "Alamat = %s\n", address)
+
+	default:
+		fmt.Fprintf(w, "Only get and post")
+	}
+}
+func main() {
+	http.HandleFunc("/", abc)
+
+	// Start server
+	fmt.Println("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
